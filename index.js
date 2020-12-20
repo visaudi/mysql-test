73c0b6ca26246a7f5de0a51a4db945a65680dd4a
Ben Pardo <benpardo@bens-mbp.attlocal.net>
2020-12-19 19:15:33 -0600

    Testing runtime environment

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 let mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'pass',
-    database: 'complexity'
-});
+/*const connection = mysql.createConnection({
+    //host: '127.0.0.1',
+    //port: '3325',
+    //user: 'ben',
+    //password: 'password',
+    //database: 'complexity'
+
+    host: process.env.HOSTNAME,
+});*/
+
+/*connection.query(
+    'INSERT INTO test (a, b, c) VALUES (3, 4, 5)',
+    function(error, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);*/
+
+console.log(process.env.HOSTNAME)
 
